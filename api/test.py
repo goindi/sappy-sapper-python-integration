7db93a376188a8bdfc210e635e91e036725cfec5
Harmeet Goindi <45494818+goindi@users.noreply.github.com>
2021-02-13 13:37:34 -0500

    Update test.py

--- api/test.py
+++ api/test.py
@@ -26,9 +26,9 @@ class handler(BaseHTTPRequestHandler):
                     my_stock = Stock(dic["sym"])
                     my_tuple = get_atm_ivol(my_stock, 7)
                     curr_price = my_stock.price
-                    low = curr_price - 1.96*my_tuple[1]
-                    high = curr_price + 1.96*my_tuple[1]
-                    message = f'{{"symbol":{my_stock.ticker},"price":{my_stock.price},"low":{low},"high":{high}}}'
+                    low = curr_price - 1.96*my_tuple[1]*curr_price
+                    high = curr_price + 1.96*my_tuple[1]*curr_price
+                    message = f'{{"symbol":{my_stock.ticker},"price":{round(my_stock.price)},"low":{round(low)},"high":{round(high)}}}'
                 except:
                     message = "-1"
         else:
