c8f8fd7b8be865c1c618ff2b6e85857f6b610070
Harmeet Goindi <45494818+goindi@users.noreply.github.com>
2021-02-13 20:45:09 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,7 +47,7 @@
 
 <script>
 	let ticker = "SPY";
-	let api_output = {"symbol":"🛁","price":"🛁","low":"🛁", "high":"🛁🛁🛁🛁"};
+	let api_output = {"symbol":"no_symbol"};
 	let py_ret = "None";
 	
 	async function handleKeydown(event) {
@@ -70,10 +70,15 @@
 	Tab or Clk
 </button>
 
-<h1>{api_output.symbol} ($ {api_output.price})</h1>
-<h3>One week range (3:1 odds):</h3>
-<h2><span style="color:red;">${api_output.low}</span> - ${api_output.high}</h2>
-
+{#if api_output.symbol == "no_symbol"}
+	<h1>Please enter a symbol to see the range of price the stock can take in a week</h1>
+{:else api_output.symbol == "no_symbol"}
+	<h1>Your symbol was invalid. Please try again</h1>
+{:else}
+	<h1>{api_output.symbol} ($ {api_output.price})</h1>
+	<h3>One week range (3:1 odds):</h3>
+	<h2><span style="color:red;">${api_output.low}</span> - ${api_output.high}</h2>
+{/if}
 <figure>
 	<img alt='Fat Tony' src='FatTony.png'>
 	<figcaption>Fat Tony: I don't get mad. I get stabby.</figcaption>
